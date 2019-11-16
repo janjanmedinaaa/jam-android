77521c7c4b79d3a6bb23e3b0243956d1e31f7d3a
Janjan Medina <medinajuanantonio95@gmail.com>
2019-11-16 17:14:51 +0800

    Add material and cardview

--- jamstarter/src/main/java/com/medina/juanantonio/jamstarter/activities/JAMActivity.kt
+++ jamstarter/src/main/java/com/medina/juanantonio/jamstarter/activities/JAMActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.medina.juanantonio.jamstarter.R
 import com.medina.juanantonio.jamstarter.common.Constants.Logs.SETUP_LOG
 import com.medina.juanantonio.jamstarter.common.Constants.SharedPreferences.BOOLEAN_PREFS
 import com.medina.juanantonio.jamstarter.common.Constants.SharedPreferences.FLOAT_PREFS
@@ -31,11 +32,19 @@ open class JAMActivity : AppCompatActivity() {
         super.onResume()
         transitioning = false
 
+        val jamContext = applicationContext
         activityOwner?.run {
             if (::sharedPrefs.isInitialized) return
             sharedPrefs = getSharedPreferences(getAppKey(), 0)
             editor = sharedPrefs.edit()
-            Log.d(SETUP_LOG, "${getAppKey()} SharedPreferences created.")
+
+            Log.d(
+                SETUP_LOG,
+                jamContext.getString(
+                    R.string.shared_prefs_created,
+                    getAppKey()
+                )
+            )
         }
     }
 
